20f4cad8723a2e4669a17b676f56b1045782d79b
arimisarn <arimisa.dev@gmail.com>
2025-07-09 15:10:46 +0300

    Add render domain to ALLOWED_HOSTS

--- backend/coaching_backend/settings.py
+++ backend/coaching_backend/settings.py
@@ -24,8 +24,12 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = [
+    "tsinjool-backend.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 # Application definition
 
