9229e40b16fe4aa4826c29023567087ac27c7289
arimisarn <arimisa.dev@gmail.com>
2025-07-09 18:01:13 +0300

    token et profile

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics; permissions
+from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from .serializers import RegisterSerializer
