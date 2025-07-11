df1574d45c7ecc2f9bcef64a8d0db561ee029307
arimisarn <arimisa.dev@gmail.com>
2025-07-11 18:07:44 +0300

    IA vocal - remove

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -1,10 +1,10 @@
-from django.urls import include, path
-from .views import chat_with_ai,conversation_list, conversation_messages
+from django.urls import  path
+from .views import chat_with_ai,conversation_list, conversation_messages, get_conversations
 
 urlpatterns = [
     path("chat/", chat_with_ai, name="chat_with_ai"),
     path("conversations/", conversation_list, name="conversation_list"),
     path("conversations/<int:conversation_id>/messages/", conversation_messages),
     # path("api/voice-chat/", VoiceChatView.as_view(), name="voice-chat"),
-    path('assistant/', include('assistant_vocal.urls')),
+    path("assistant/", get_conversations, name="assistant"),
     ]
