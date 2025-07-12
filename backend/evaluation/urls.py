10307076c3ddf4be55c536b0b44210b7c3e71c5b
arimisarn <arimisa.dev@gmail.com>
2025-07-12 11:27:52 +0300

    Evaluation

--- backend/evaluation/urls.py
+++ backend/evaluation/urls.py
@@ -1,23 +1,24 @@
 from django.urls import path
 
-from accounts import views
+# from accounts import views
+from .views import check_assessment_status, get_assessment_questions, get_coaching_path, submit_assessment
 
 urlpatterns = [
     # path('evaluation/', EvaluationView, name='evaluation'),
     # path("evaluation/last/", latest_evaluation),
         path('assessment/questions/<str:coaching_type>/', 
-         views.get_assessment_questions, 
+         get_assessment_questions, 
          name='get_assessment_questions'),
     
     path('assessment/submit/', 
-         views.submit_assessment, 
+         submit_assessment, 
          name='submit_assessment'),
     
     path('coaching-path/', 
-         views.get_coaching_path, 
+         get_coaching_path, 
          name='get_coaching_path'),
     
     path('assessment/status/', 
-         views.check_assessment_status, 
+         check_assessment_status, 
          name='check_assessment_status'),
 ]
