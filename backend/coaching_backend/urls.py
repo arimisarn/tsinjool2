a786d581f8f38d354924c94db820fc499f385b1e
arimisarn <arimisa.dev@gmail.com>
2025-07-13 13:58:23 +0300

    Evaluation

--- backend/coaching_backend/urls.py
+++ backend/coaching_backend/urls.py
@@ -1,17 +1,23 @@
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    
-    # Donne des préfixes différents à chaque app :
-    path('api/accounts/', include('accounts.urls')),
-    path('api/chat/', include('chat.urls')),
-    path('api/evaluation/', include('evaluation.urls')),
-]
 
-# Sert les fichiers media en DEBUG
+# Vue simple pour la racine
+def health_check(request):
+    return JsonResponse(
+        {"status": "ok", "message": "🎯 Le backend Django sur Render fonctionne !"}
+    )
+
+
+urlpatterns = [
+    path("", health_check),  # ← Vue pour la racine
+    path("admin/", admin.site.urls),
+    path("api/", include("accounts.urls")),  # 👈 Ajouté ici
+    path("api/", include("chat.urls")),
+    path("api/", include("evaluation.urls")),  # Ajoute ça
+]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
