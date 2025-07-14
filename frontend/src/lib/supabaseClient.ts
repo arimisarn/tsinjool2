4d6bb9e7a22bc5bf7fd545259299892701cc9f40
arimisarn <arimisa.dev@gmail.com>
2025-07-14 14:53:50 +0300

    photo

--- frontend/src/lib/supabaseClient.ts
+++ frontend/src/lib/supabaseClient.ts
@@ -1,10 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 console.log(supabaseUrl);
-console.log("SUPABASE_ANON_KEY =", import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+console.log(supabaseAnonKey);
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("supabaseUrl or supabaseAnonKey is missing.");
 }
