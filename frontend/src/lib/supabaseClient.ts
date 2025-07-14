59aeffa689814e97f6cba31743bb96b0565a8dbc
arimisarn <arimisa.dev@gmail.com>
2025-07-14 14:43:06 +0300

    photo

--- frontend/src/lib/supabaseClient.ts
+++ frontend/src/lib/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-// ✅ Ces deux valeurs doivent être définies correctement
-const supabaseUrl = import.meta.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseUrl = import.meta.env.NEXT_PUBLIC_SUPABASE_UR;
 const supabaseAnonKey = import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
