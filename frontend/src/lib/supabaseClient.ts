603299a65e830689ff17ba0b832e8613bfaf3c0a
arimisarn <arimisa.dev@gmail.com>
2025-07-14 14:06:38 +0300

    photo

--- frontend/src/lib/supabaseClient.ts
+++ frontend/src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ezetxacdchfymubmdbnw.supabase.co'  // remplace par ton URL Supabase
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6ZXR4YWNkY2hmeW11Ym1kYm53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI0ODY5MzcsImV4cCI6MjA2ODA2MjkzN30.W0BooeBP0kjyU7_Qg9MBZJx23osvtudo20b8xNkI48U'  // remplace par ta clé publique (anon key)
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
