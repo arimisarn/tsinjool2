0de459394df2c5dbe566e8bc507b8646485f8377
arimisarn <arimisa.dev@gmail.com>
2025-07-15 14:18:04 +0300

    modification de coatching my

--- backend/evaluation/signals.py
+++ backend/evaluation/signals.py
@@ -1,11 +1,11 @@
+# accounts/signals.py
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Profile, CoachingPath
-from django.conf import settings
+from .models import Profile
+from coaching.models import CoachingPath
 
 
 @receiver(post_save, sender=Profile)
-def create_coaching_path_for_user(sender, instance, created, **kwargs):
-    user = instance.user
-    if not CoachingPath.objects.filter(user=user).exists():
-        CoachingPath.objects.create(user=user)
+def create_user_path(sender, instance, created, **kwargs):
+    if created:
+        CoachingPath.objects.get_or_create(user=instance.user)
