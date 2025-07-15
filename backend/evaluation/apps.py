95dbaed56a35bd63a5c55dfb70d7f4fb4c5b2691
arimisarn <arimisa.dev@gmail.com>
2025-07-15 14:06:44 +0300

    modification de coatching my

--- backend/evaluation/apps.py
+++ backend/evaluation/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
 
+
 class CoachingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'evaluation'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "evaluation"
+
+
+def ready(self):
+    import evalution.signals  # si ton signal est dans l’app `accounts`
