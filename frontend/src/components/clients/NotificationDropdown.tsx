9eeef91b41944d566ade1d838c4de4d88628d4aa
arimisarn <arimisa.dev@gmail.com>
2025-07-18 16:35:09 +0300

    planned exerice

--- frontend/src/components/clients/NotificationDropdown.tsx
+++ frontend/src/components/clients/NotificationDropdown.tsx
@@ -111,9 +111,9 @@ export default function NotificationDropdown() {
         {unreadCount > 0 && (
           <span
             className="absolute -top-1 -right-1 text-xs text-white w-5 h-5 rounded-full flex items-center justify-center
-             bg-gradient-to-br from-blue-500 to-purple-500 
+             bg-gradient-to-br from-blue-600 to-purple-600 
              dark:from-blue-400 dark:to-purple-600 
-             shadow-md animate-pulse"
+             shadow-md"
           >
             {unreadCount}
           </span>
